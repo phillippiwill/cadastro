59cf6fd3333fc7d81e73cafcb81316ded4e7a061
phillippiwill <80370661+phillippiwill@users.noreply.github.com>
2024-11-27 14:09:01 -0300

    Update settings.py

--- contatos/settings.py
+++ contatos/settings.py
@@ -132,3 +132,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
